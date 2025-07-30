0717bbf9cbe1717bfc3538544fa59c552c72a10c
goodwinhavard <havard.goodwin@gmail.com>
2025-07-30 18:36:39 +0200

    Update 3_🌍_Select_Travel.py

--- pages/3_🌍_Select_Travel.py
+++ pages/3_🌍_Select_Travel.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Select entry")
 st.caption("This page allows you to see a travel.")
+
+if "travel_entries" in st.session_state and st.session_state.travel_entries:
+    entry_names = [entry.get_name() for entry in st.session_state.travel_entries]
+    selected_entry = st.selectbox("Select a travel entry", entry_names)
+else:
+    st.write("No travel entries found.")
