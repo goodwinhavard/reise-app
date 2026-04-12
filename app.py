711d093a4639f5514533fc58d8a20423dcd8953d
Håvard Goodwin <havard.goodwin@gmail.com>
2026-04-12 17:08:19 +0200

    Front page.

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from data.sidebar import render_sidebar
 
 st.set_page_config(
-    page_title="Håvard's Travel App",
+    page_title="Goodwins Travel App",
     page_icon=":airplane:"
 )
 
-st.title("Håvard's Travel App")
+st.title("Goodwins Travel App")
 
 render_sidebar()
